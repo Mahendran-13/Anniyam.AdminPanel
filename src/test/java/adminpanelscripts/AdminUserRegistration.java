e52f80c529eca033c794445ff9d7a628f6413971
mahen <mahendranvsm13@gmail.com>
2024-09-16 17:38:51 +0530

    scripts are commanded

--- src/test/java/adminpanelscripts/AdminUserRegistration.java
+++ src/test/java/adminpanelscripts/AdminUserRegistration.java
@@ -23,29 +23,29 @@ public class AdminUserRegistration {
 		driver.findElement(By.xpath("//span[text()='User Registration ']")).click();
 		
 		Thread.sleep(3000);
-		driver.findElement(By.xpath("//input[@id='firstName']")).sendKeys("mahendran");
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys("V");
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("mahen13@gmail.com");
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//input[@id='mobile']")).sendKeys("9876451230");
-		WebElement groupdropdown =driver.findElement(By.xpath("//span[@class='mat-select-placeholder mat-select-min-line ng-tns-c53-11 ng-star-inserted']"));
-		groupdropdown.click();
-		driver.findElement(By.xpath("//span[text()=' Office Staffs']")).click();
-		WebElement roledropdown=driver.findElement(By.xpath("//span[@class='mat-select-placeholder mat-select-min-line ng-tns-c53-13 ng-star-inserted']"));
-		roledropdown.click();
-		driver.findElement(By.xpath("//span[text()=' Support ']")).click();
-		WebElement menudropdown= driver.findElement(By.xpath("//mat-expansion-panel-header[@id='mat-expansion-panel-header-0']"));
-		menudropdown.click();
-
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		WebElement drop= driver.findElement(By.xpath("//mat-panel-title[text()=' Reports ']"));
-		Thread.sleep(2000);
-		js.executeScript("arguments[0].scrollIntoView();", drop);
-		Thread.sleep(6000);
-
-		//driver.findElement(By.xpath("//span[text()='Register']")).click();
+//		driver.findElement(By.xpath("//input[@id='firstName']")).sendKeys("mahendran");
+//		Thread.sleep(3000);
+//		driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys("V");
+//		Thread.sleep(3000);
+//		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("mahen13@gmail.com");
+//		Thread.sleep(3000);
+//		driver.findElement(By.xpath("//input[@id='mobile']")).sendKeys("9876451230");
+//		WebElement groupdropdown =driver.findElement(By.xpath("//span[@class='mat-select-placeholder mat-select-min-line ng-tns-c53-11 ng-star-inserted']"));
+//		groupdropdown.click();
+//		driver.findElement(By.xpath("//span[text()=' Office Staffs']")).click();
+//		WebElement roledropdown=driver.findElement(By.xpath("//span[@class='mat-select-placeholder mat-select-min-line ng-tns-c53-13 ng-star-inserted']"));
+//		roledropdown.click();
+//		driver.findElement(By.xpath("//span[text()=' Support ']")).click();
+//		WebElement menudropdown= driver.findElement(By.xpath("//mat-expansion-panel-header[@id='mat-expansion-panel-header-0']"));
+//		menudropdown.click();
+//
+//		JavascriptExecutor js = (JavascriptExecutor) driver;
+//		WebElement drop= driver.findElement(By.xpath("//mat-panel-title[text()=' Reports ']"));
+//		Thread.sleep(2000);
+//		js.executeScript("arguments[0].scrollIntoView();", drop);
+//		Thread.sleep(6000);
+//
+//		//driver.findElement(By.xpath("//span[text()='Register']")).click();
 		return driver;
 
 
